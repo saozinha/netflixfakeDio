25d7110d95258c22dbbd8b2155b86f428328a55b
ConceicaoLourenco <conceicaolourenzo@gmail.com>
2020-11-12 14:28:55 -0300

    add carousel of movie

---
+++ js/owl/images.js
@@ -0,0 +1,28 @@
+{
+    "items" : [
+      {
+        "img": "../img/boxfilmes/mini1.jpg",
+        "alt" : "Owl Image 1"
+      },
+      {
+        "img": "../img/mini2.jpg",
+        "alt" : "Owl Image 2"
+      },
+      {
+        "img": "../img/mini3.jpg",
+        "alt" : "Owl Image 1"
+      },
+      {
+        "img": "../img/mini4.jpg",
+        "alt" : "Owl Image 2"
+      },
+      {
+        "img": "../img/mini5.jpg",
+        "alt" : "Owl Image 1"
+      },
+      {
+        "img": "../img/mini6.jpg",
+        "alt" : "Owl Image 2"
+      }
+    ]
+  }
---
+++ js/owl/setup.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+ 
+    $("#owl-demo").owlCarousel({
+      jsonPath : 'js/owl/json/custom.json',
+      jsonSuccess : customDataSuccess
+    });
+   
+    function customDataSuccess(data){
+      var content = "";
+      for(var i in data["items"]){
+         
+         var img = data["items"][i].img;
+         var alt = data["items"][i].alt;
+   
+         content += "<img src=\"" +img+ "\" alt=\"" +alt+ "\">"
+      }
+      $("#owl-demo").html(content);
+    }
+   
+  }); 
